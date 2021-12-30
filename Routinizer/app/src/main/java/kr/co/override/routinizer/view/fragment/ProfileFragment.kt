e3ceca9574b45d2e9ab7f45bc25c27b31950bae9
LucyMoon <81845462+LucyMoon@users.noreply.github.com>
2021-12-30 09:07:07 +0900

    commit

--- Routinizer/app/src/main/java/kr/co/override/routinizer/view/fragment/ProfileFragment.kt
+++ Routinizer/app/src/main/java/kr/co/override/routinizer/view/fragment/ProfileFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import kr.co.override.routinizer.R
 import kr.co.override.routinizer.databinding.FragmentProfileBinding
 import kr.co.override.routinizer.databinding.FragmentRankBinding
@@ -33,6 +34,43 @@ class ProfileFragment : Fragment() {
             getMyProfile()
             onSuccessEvent.observe(this@ProfileFragment, {
                 binding.tvName.text = name.value
+                binding.tvPoint.text = point.value
+                binding.tvSchool.text = school.value
+                binding.tvPlayingC.text = continuous.value.toString()
+                binding.tvPlaying.text = count.value.toString()
+                var reImage: String = "http://192.168.52.67:4000/uploads/${avatar.value}"
+                Glide.with(binding.root)
+                    .load(reImage)
+                    .error(R.drawable.default_user)
+                    .centerCrop()
+                    .into(binding.ivProfile)
+                when(grade.value){
+                    -2 -> {
+                        binding.tvTier.text = "히키코모리"
+                        binding.ivTier.setImageResource(R.drawable.ic_hiki_h)
+                    }
+                    -1 -> {
+                        binding.tvTier.text = "게으름뱅이"
+                        binding.ivTier.setImageResource(R.drawable.ic_lazy_h)
+                    }
+                    0 -> {
+                        binding.tvTier.text = "일반인"
+                        binding.ivTier.setImageResource(R.drawable.ic_human_h)
+                    }
+                    1 -> {
+                        binding.tvTier.text = "성실이"
+                        binding.ivTier.setImageResource(R.drawable.ic_head_h)
+                    }
+                    2 -> {
+                        binding.tvTier.text = "바른생활 루틴이"
+                        binding.ivTier.setImageResource(R.drawable.ic_rou_h)
+                    }
+                    3 -> {
+                        binding.tvTier.text = "불붙은 챌린지"
+                        binding.ivTier.setImageResource(R.drawable.ic_god_h)
+                    }
+                }
+
             })
         }
 
